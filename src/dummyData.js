e3da5fd97e6d1b59af6e86a08b1962b41239373c
Cenusa Dumitru <dumitru.cenusa@assist.ro>
2021-03-29 16:15:03 +0300

    Initial commit

---
+++ src/dummyData.js
@@ -0,0 +1,32 @@
+export const CARDS_LIST = [
+  {
+    title: 'Wolf Hall',
+    imageUrl: 'https://i.guim.co.uk/img/media/c2960ddd52aae3d09bfc3595d786f9349d3f9a46/0_104_6720_4032/master/6720.jpg?width=620&quality=45&auto=format&fit=max&dpr=2&s=3817f4c422d332e685d8ba77d57852ba',
+    description: "Mantel had been publishing for a quarter century before the project that made her a phenomenon, set to be concluded with the third part of the trilogy, The Mirror and the Light, next March. To read her story of the rise of Thomas Cromwell at the Tudor court, detailing the making of a new England and the self-creation of a new kind of man, is to step into the stream of her irresistibly authoritative present tense and find oneself looking out from behind her hero’s eyes. ",
+  },
+  {
+    title: 'Austerlitz',
+    imageUrl: 'https://i.guim.co.uk/img/media/251e3c314174b1eb1c80830f3f66fb89a804be6e/0_66_3216_1930/master/3216.jpg?width=940&quality=45&auto=format&fit=max&dpr=2&s=0bb81f1c0283e203a06c819369a83d14',
+    description: "Sebald died in a car crash in 2001, but his genre-defying mix of fact and fiction, keen sense of the moral weight of history and interleaving of inner and outer journeys have had a huge influence on the contemporary literary landscape. His final work, the typically allusive life story of one man, charts the Jewish disapora and lost 20th century with heartbreaking power",
+  },
+  {
+    title: 'Between the World and Me',
+    imageUrl: 'https://i.guim.co.uk/img/media/4da3769dd9c85045fd2d8cafeec8d34fa45cc10f/0_167_2218_1330/master/2218.jpg?width=380&quality=45&auto=format&fit=max&dpr=2&s=ec502ea1299291eb582f856a3354dedb',
+    description: "Coates’s impassioned meditation on what it means to be a black American today made him one of the country’s most important intellectuals and writers. Having grown up the son of a former Black Panther on the violent streets of Baltimore, he has a voice that is challenging but also poetic. ",
+  },
+  {
+    title: 'Half of a Yellow Sun',
+    imageUrl: 'https://i.guim.co.uk/img/media/03865ae1ff9028ef6f7d43591450141f87984066/0_138_2912_1747/master/2912.jpg?width=620&quality=45&auto=format&fit=max&dpr=2&s=4154d18646b29a4702461d5e1e1ad20b',
+    description: "When Nigerian author Adichie was growing up, the Biafran war 'hovered over everything'. Her sweeping, evocative novel, which won the Orange prize, charts the political and personal struggles of those caught up in the conflict and explores the brutal legacy of colonialism in Africa.",
+  },
+  {
+    title: 'Fingersmith',
+    imageUrl: 'https://i.guim.co.uk/img/media/68948160deed5971a25ecbd0379fa880bb2895d6/115_0_2856_1714/master/2856.jpg?width=380&quality=45&auto=format&fit=max&dpr=2&s=82a115f11fe22edda611c594f7414b8b',
+    description: "Moving from the underworld dens of Victorian London to the boudoirs of country house gothic, and hingeing on the seduction of an heiress, Waters’s third novel is a drippingly atmospheric thriller, a smart study of innocence and experience, and a sensuous lesbian love story – with a plot twist to make the reader gasp.",
+  },
+  {
+    title: 'The Shock Doctrine',
+    imageUrl: 'https://i.guim.co.uk/img/media/43bfa67fdb96d3f0dfe26976c0000617569839e8/0_53_3504_2103/master/3504.jpg?width=940&quality=45&auto=format&fit=max&dpr=2&s=d2f4075abf52a27c91dee76c4290faa2',
+    description: "In this urgent examination of free-market fundamentalism, Klein argues – with accompanying reportage – that the social breakdowns witnessed during decades of neoliberal economic policies are not accidental, but in fact integral to the functioning of the free market, which relies on disaster and human suffering to function.",
+  }
+]
